02d137a0f174b771ee9005e67dc9a15fa56cbb02
Natália Alves <natalia.alvescam@gmail.com>
2021-09-30 18:20:18 -0300

    adiciona o update e o read no pet.routes

--- routes/pet.routes.js
+++ routes/pet.routes.js
@@ -16,4 +16,39 @@ router.post("/registration", (req, res) => {
     });
 });
 
+//Read => Pet List
+router.get("/registration", (req, res) => {
+  PetModel.find()
+    .then((result) => {
+      res.status(200).json(result);
+    })
+    .catch((err) => {
+      console.error(err);
+      return res.status(500).json({
+        msg: "Falha ao encontrar pets: erro interno no servidor.",
+      });
+    });
+});
+
+//Update Pet info
+router.patch("/registration/:id", async (req, res) => {
+  PetModel.findOneAndUpdate(
+    { _id: req.params.id },
+    { $set: { ...req.body } },
+    { new: true, runValidators: true }
+  )
+    .then((result) => {
+      if (!result) {
+        return res.status(404).json({ msg: "Pet não encontrado!" });
+      }
+      return res.status(200).json(result);
+    })
+    .catch((err) => {
+      console.error(err);
+      return res.status(500).json({
+        msg: "Falha ao encontrar pets: erro interno no servidor.",
+      });
+    });
+});
+
 module.exports = router;
