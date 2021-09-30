d41ebd64e37f3e963eb779502a7cacc2167f773e
Natália Alves <natalia.alvescam@gmail.com>
2021-09-30 16:58:37 -0300

    adiciona rota nova no app.js

--- app.js
+++ app.js
@@ -4,13 +4,22 @@ const cors = require("cors");
 require("./config/db.config")();
 
 const app = express();
+const API_VERSION = 1;
 
 app.use(express.json());
 // Não esquecer de criar variável de ambiente com o endereço do seu app React (local ou deployado no Netlify)
 app.use(cors({ origin: process.env.REACT_APP_URL }));
 
+
+//Redirecionando as requisições para os roteadores
+
+const basePath = `/api/v${API_VERSION}`
+
 const userRouter = require("./routes/user.routes");
-app.use("/api", userRouter);
+app.use(basePath, userRouter);
+
+const petRouter = require("./routes/pet.routes");
+app.use(basePath, petRouter);
 
 app.listen(Number(process.env.PORT), () =>
   console.log(`Server up and running at port ${process.env.PORT}`)
